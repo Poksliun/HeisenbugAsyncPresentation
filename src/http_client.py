fe7951b6cc9337a944334919ce0316d710fafafa
avnekr16 <avnekr16@mts.ru>
2024-09-05 04:37:29 +0200

    add: Добавлены "красивые" тесты

---
+++ src/http_client.py
@@ -0,0 +1,30 @@
+import asyncio
+from typing import Optional
+
+import aiohttp
+from pydantic import BaseModel
+
+
+class UserProperty(BaseModel):
+    name: str
+    age: int
+    property_type: str
+
+
+class HttpClient:
+    """Http клиент для отправки запросов на сервер
+    """
+
+    def __init__(self, domain: str, _delay: int = 1):
+        self._domain: str = domain
+        self._delay: int = _delay
+
+    async def user_write(self, session: aiohttp.ClientSession, data: UserProperty) -> Optional[int]:
+        url: str = self._domain + '/write'
+        async with session.post(
+                url=url,
+                json=data.dict()
+        ) as response:
+            resp_body: Optional[dict] = await response.json()
+            await asyncio.sleep(self._delay)
+            return resp_body.get('id')
---
+++ tests/test_pretty_users.py
@@ -0,0 +1,54 @@
+import random
+
+import pytest
+
+from db.async_select import (
+    async_get_user_task,
+    async_get_user_property_task
+)
+
+from src.http_client import (
+    HttpClient,
+    UserProperty
+)
+
+MANY_TEST_COUNT = 100
+
+
+def create_random_number() -> int:
+    return random.randint(0, 99999)
+
+
+class TestUsers:
+    WRITE_END_POINT = '/write'
+    MIN_AGE = 20
+    MAX_AGE = 40
+    PROPERTY_TYPE = 'flat'
+
+    @pytest.mark.parametrize('iterator', list(range(0, MANY_TEST_COUNT)))
+    @pytest.mark.asyncio_cooperative
+    async def test_async_pretty_write_user(
+            self,
+            check,
+            domain,
+            iterator,
+            async_db,
+            session
+    ):
+        # Arrange
+        name: str = f'test_{create_random_number():0>4}'
+        age: int = random.randint(self.MIN_AGE, self.MAX_AGE)
+        data = UserProperty(name=name, age=age, property_type=self.PROPERTY_TYPE)
+        client = HttpClient(domain)
+        # Act
+        user_id = await client.user_write(session, data=data)
+        # Assert
+        user = async_get_user_task(async_db, user_id)
+        user_property = async_get_user_property_task(async_db, user_id)
+        with check:
+            user = await user
+            assert user[1] == name
+            assert user[2] == age
+        with check:
+            user_property = await user_property
+            assert user_property[1] == self.PROPERTY_TYPE
